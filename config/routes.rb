ce0081e1e40afd33d661dcd38df789555f34a1f0
Shruti <shruti@MacBookPro2.local>
2017-12-03 04:24:52 +0100

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  post 'poll/get/org' => 'polls#get_org'
+  post 'poll/org' => 'polls#get_org'
   post 'poll/get' => 'polls#get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
