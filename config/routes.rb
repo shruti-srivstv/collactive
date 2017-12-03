54bb19839501c8cc5edc6c5aebc3a4c5ab9355c7
Shruti <shruti@MacBookPro2.local>
2017-12-03 08:23:32 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   post 'poll/org' => 'polls#get_org'
-  post 'poll/get' => 'polls#get'
+  get 'poll/:poll_id' => 'polls#get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
